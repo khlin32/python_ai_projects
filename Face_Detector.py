910371ff642568f11a0cb23e7cc24238503dd74e
jamesl <jlin@jblearning.com>
2021-02-16 21:42:32 -0500

    another update

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,1 +1,16 @@
+import cv2
+#from random import randrange
+
+# Load some pre-trained data on face frontals from opencv (haar cascade algorithm)
+# trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Choose an image to detect face in
+img = cv2.imread('TomBrady.jpg')
+
+# Display the image
+cv2.imshow("Face Detected", img)
+
+# Wait a bit
+cv2.waitKey()
+
 print("Code Completed")
